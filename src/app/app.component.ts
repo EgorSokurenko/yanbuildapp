0c13f864d676d5b117ef83ec182b82ee9726a740
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-05-04 12:51:57 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,4 @@ export class AppComponent {
    isMobile: boolean;
    isOpen: boolean = false; 
 
-  constructor() {
-    this.checkScreenSize();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.checkScreenSize();
-   }
-   
-   @HostListener('window:scroll', ['$event'])
-  onScroll(event) {
-    if (window.scrollX > 0) { // перевірка, чи прокрутка відбувається вліво
-      this.isOpen = false; // закриваємо модальне вікно
-    }
-  }
-
-  checkScreenSize() {
-    this.isMobile = window.innerWidth < 800;
-   }
-   
- openMenu() {
-    this.isOpen = !this.isOpen;
-  }
 }
