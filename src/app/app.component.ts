f4a78ec2e20c5c6db762beac2a5dacbcbd6c614d
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-05-04 12:47:54 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component, HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  isMobile: boolean;
+   isMobile: boolean;
+   isOpen: boolean = false; 
 
   constructor() {
     this.checkScreenSize();
@@ -16,9 +17,15 @@ export class AppComponent {
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.checkScreenSize();
-  }
+   }
+   
+   @HostListener('window:scroll', ['$event'])
 
   checkScreenSize() {
     this.isMobile = window.innerWidth < 800;
+   }
+   
+ openMenu() {
+    this.isOpen = !this.isOpen;
   }
 }
